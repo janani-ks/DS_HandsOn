988adea7eab05a177c2916edadf91662620ac8f9
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-02-07 10:06:50 +0530

    Add files via upload

---
+++ simple/src/main/java/calculator/Main.java
@@ -0,0 +1,70 @@
+package calculator;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+class SimpleCalculator{
+	public double ans;
+}
+class Addition extends SimpleCalculator{
+    double l;
+	public void calculate(double a,double b) {
+		l = a+b;
+	}
+}
+class Subraction extends SimpleCalculator{
+    double l;
+    public void calculate(double a,double b) {
+    	l = a-b;
+	}
+}
+class Multiplication extends SimpleCalculator{
+    double l;
+    public void calculate(double a,double b) {
+    	l = a*b;
+	}
+}
+class Division extends SimpleCalculator{
+     double l;
+    public void calculate(double a,double b) {
+    	l = a/b;
+	}
+}
+public class Main {
+	public static void main(String[] args) {
+		Logger l = Logger.getLogger("com.api.jar");
+		Scanner c = new Scanner(System.in);
+		l.info("Enter two values ");
+		Float a = c.nextFloat();
+		Float b = c.nextFloat();
+		l.info("Enter the operation ");
+		char o = c.next().charAt(0); 
+		SimpleCalculator s = new SimpleCalculator();
+		switch(o){
+		case '+':
+			Addition a1 = new Addition();
+			a1.calculate(a,b);
+			s.ans = a1.l;
+			break;
+		case '-':
+			Subraction a2 = new Subraction();
+			a2.calculate(a,b);
+			s.ans = a2.l;
+			break;
+		case '*':
+			Multiplication a3 = new Multiplication();
+			a3.calculate(a,b);
+			s.ans = a3.l;
+			break;
+		case '/':
+			Division a4 = new Division();
+			a4.calculate(a,b);
+			s.ans = a4.l;
+			break;
+		default:
+			l.info("Invalid Operation");
+			break;
+		}
+		l.log(Level.INFO,()-> "The Answer is = "+s.ans);
+}
+}
